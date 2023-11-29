244edfb7094aa2d8001c065158aaa27a95460c62
sanchez-alyssa <ww7aly@gmail.com>
2023-11-29 08:58:17 +0800

    add all

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "./layout/layout"
+import Layout from "./layout/Layout"
 import Home from "./components/Home"
 import About from "./components/About"
 import Project from "./components/Project"
