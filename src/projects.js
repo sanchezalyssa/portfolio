c351dfeb0bd57c56ac57ab80cbf5a561f864a49b
sanchez-alyssa <ww7aly@gmail.com>
2023-11-29 09:21:25 +0800

    move img to public

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,6 @@
 export const projects = [
     {
-        img: "public/blog.jpg",
+        img: "/blog.jpg",
         title: "Random Blog Post",
         detail: " Created a user-friendly blog feature with React JS, enabling easy composition and storage of posts with Local Storage.Ensured mobile responsiveness for a seamless experience across devices. Ongoing collaboration for continuous improvements.",
         id: "1",
@@ -14,7 +14,7 @@ export const projects = [
         ],
     },
     {
-        img: "/public/scheme.jpg",
+        img: "/scheme.jpg",
         title: "Color Scheme",
         detail: "Color API to create a user-friendly tool for designers and front-end developers.",
         id: "2",
