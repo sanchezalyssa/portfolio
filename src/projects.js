2e314f881db1b4ea83186fa5a8fb25dbe178f789
sanchez-alyssa <ww7aly@gmail.com>
2023-11-29 09:29:26 +0800

    move img folder

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,6 @@
-export const projects = [
+const projects = [
     {
-        img: "/blog.jpg",
+        img: "image/blog.jpg",
         title: "Random Blog Post",
         detail: " Created a user-friendly blog feature with React JS, enabling easy composition and storage of posts with Local Storage.Ensured mobile responsiveness for a seamless experience across devices. Ongoing collaboration for continuous improvements.",
         id: "1",
@@ -14,7 +14,7 @@ export const projects = [
         ],
     },
     {
-        img: "/scheme.jpg",
+        img: "image/scheme.jpg",
         title: "Color Scheme",
         detail: "Color API to create a user-friendly tool for designers and front-end developers.",
         id: "2",
@@ -28,3 +28,4 @@ export const projects = [
         ],
     },
 ]
+export default projects
