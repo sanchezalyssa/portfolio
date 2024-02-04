bce2d55cb4756e2e15df6e65002a6d8b68ef65ab
sanchez-alyssa <ww7aly@gmail.com>
2024-02-04 12:42:19 +0800

    adjust tech stack text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,51 +13,48 @@ import avatar from "/image/avatar1.jpg"
 
 import Typewriter from "typewriter-effect"
 export default function Home() {
-    return (
-        <div className="home">
-            <img className="avatar" src={avatar} alt="avatar" />
-            <div className="about-me">
-                <h1>
-                    <Typewriter
-                        options={{
-                            autoStart: true,
-                            loop: true,
-                            delay: 100,
-                            strings: ["Front-end<br>Web developer👋"],
-                            // or use an array if you have multiple lines
-                            // strings: ["Front-end", "Web developer 👋"],
-                        }}
-                    />
-                </h1>
+  return (
+    <div className="home">
+      <img className="avatar" src={avatar} alt="avatar" />
+      <div className="about-me">
+        <h1>
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+              delay: 100,
+              strings: ["Front-end<br>Web developer👋"],
+              // or use an array if you have multiple lines
+              // strings: ["Front-end", "Web developer 👋"],
+            }}
+          />
+        </h1>
 
-                <p>
-                    Hey there! I'm Alyssa, a front-end developer who loves turning ideas into web magic. Proficient in HTML, CSS, JavaScript and
-                    React.
-                </p>
-                <div className="icons">
-                    <a className="social" target="_blank" rel="noreferrer" href="https://github.com/sanchezalyssa">
-                        <FaGithub className="social" to />
-                    </a>
-                    <a className="social" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/alyssa-sanchez-061a5a265/">
-                        <FaLinkedin className="social" />
-                    </a>
-                </div>
-                <div className="tech">
-                    <div className="tech-title">
-                        <h3>Tech Stack</h3>
-                    </div>
-                    <div className="tech-img">
-                        <img src={html} alt="html" />
-                        <img src={css} alt="css" />
-                        <img src={js} alt="html" />
-                        <img src={react} alt="react" />
-                        <img src={bootstrap} alt="bootstrap" />
-                        <img src={tailwind} alt="tailwind" />
-                        <img src={node} alt="node" />
-                        <img src={mysql} alt="mysql" />
-                    </div>
-                </div>
-            </div>
+        <p>Hey there! I'm Alyssa, a front-end developer who loves turning ideas into web magic. Proficient in HTML, CSS, JavaScript and React.</p>
+        <div className="icons">
+          <a className="social" target="_blank" rel="noreferrer" href="https://github.com/sanchezalyssa">
+            <FaGithub className="social" to />
+          </a>
+          <a className="social" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/alyssa-sanchez-061a5a265/">
+            <FaLinkedin className="social" />
+          </a>
         </div>
-    )
+        <div className="tech">
+          <div className="tech-title">
+            <h3>Tech Stack</h3>
+          </div>
+          <div className="tech-img">
+            <img src={html} alt="html" />
+            <img src={css} alt="css" />
+            <img src={js} alt="html" />
+            <img src={react} alt="react" />
+            <img src={bootstrap} alt="bootstrap" />
+            <img src={tailwind} alt="tailwind" />
+            <img src={node} alt="node" />
+            <img src={mysql} alt="mysql" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
